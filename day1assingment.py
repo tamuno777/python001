7a3a3c6964bed41d0ae93be6717aaddbe449f26c
winniedavis <63395082+tamuno777@users.noreply.github.com>
2022-04-13 13:24:49 +0100

    Add files via upload

---
+++ day1assingment.py
@@ -0,0 +1,17 @@
+print('MY NAME IS JAMES')
+STATE = 'I\'m from a well-known village with good culture in kogi state'# decleared a variable called state assinging a string value of i'm from a well-known village with good culture in kogi state
+print(STATE)
+
+TM = 1000 # decleared a variable call TM assinging an intergere value of 1000
+Q = 3
+P = 450
+statement = 'I have {} dollars so i can buy {} football for {}.oo dollars.'
+print (statement.format(TM,Q,P))
+
+POEM = '''
+our life is like,
+a thorny rose
+not perfect,
+but always beutiful.
+''' # This is a multiple string
+print(POEM.upper())
